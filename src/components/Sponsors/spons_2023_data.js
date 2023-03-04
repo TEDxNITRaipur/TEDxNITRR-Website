a31a6e0ab898dae9c46201548f21407f35c025e5
Rohit <rohitmondall8000@gmail.com>
2023-03-04 18:18:11 +0530

    Final edit

--- src/components/Sponsors/spons_2023_data.js
+++ src/components/Sponsors/spons_2023_data.js
@@ -1,4 +1,4 @@
-import cspgl from "./2023_spons_img/CSPGCL.jpeg"
+import csptcl from "./2023_spons_img/csptcl.jpeg"
 import A1_img from "./2023_spons_img/A1.jpeg"
 import asian_news_img from "./2023_spons_img/asian_news.jpeg"
 import babylon_img from "./2023_spons_img/babylon.jpeg"
@@ -12,89 +12,113 @@ import print_mine_img from "./2023_spons_img/print_mine.png"
 import sparkcars_img from "./2023_spons_img/sparkcars.webp"
 import AB_img from "./2023_spons_img/AB.jpeg"
 import dr_ujj_img from "./2023_spons_img/dr_ujjwala.png"
+import fashionkart_img from "./2023_spons_img/fashionkart.jpg"
 import rocks_img from "./2023_spons_img/on_the_rocks.png"
+import graceful from "./2023_spons_img/graceful_media.webp"
 import three_cheers from "./2023_spons_img/3_cheers.jpeg"
 
 const SponsData =[
     
     {
         id: 1,
-        sponsImg: cspgl,
+        sponsImg: csptcl,
+        sponsAbt: "We are happy to announce CSPTCL as our platinum partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 2,
         sponsImg: A1_img,
+        sponsAbt: "We are happy to announce A1 bakery as our bakery partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 3,
         sponsImg: asian_news_img,
+        sponsAbt: "We are happy to announce Asian News as our news partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 4,
         sponsImg: babylon_img,
+        sponsAbt: "We are happy to announce Hotel Babylon International as our hospitality partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 5,
-        sponsImg: dd_img,
+        sponsImg: fashionkart_img,
+        sponsAbt: "We are happy to announce Fashion Kart as our fashion partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 6,
         sponsImg: hilers_img,
+        sponsAbt: "We are happy to announce Hilers as our food partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 7,
         sponsImg: infinito_img,
+        sponsAbt: "We are happy to announce Infinito Comics as our comic media partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 8,
         sponsImg: news24_img,
+        sponsAbt: `We are happy to announce News 24 as our sponsor for TEDxNITRaipur 6.0.`
     },
 
     {
         id: 9,
         sponsImg: ongo_img,
+        sponsAbt: "We are happy to announce Ongo water as our sponsor for TEDxNITRaipur 6.0."
     },
 
     {
         id: 10,
         sponsImg: prasiddhi_img,
+        sponsAbt: "We are happy to announce Prasiddhi as our EM partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 11,
         sponsImg: print_mine_img,
+        sponsAbt: "We are happy to announce PrintMine as our gifting partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 12,
         sponsImg: sparkcars_img,
+        sponsAbt: "We are happy to announce Sparkcars as our travel partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 13,
-        sponsImg: AB_img
+        sponsImg: AB_img,
+        sponsAbt: "We are happy to announce AB Classes as our education partner for TEDxNITRaipur 6.0."
     },
 
     {
         id: 14,
-        sponsImg: dr_ujj_img
+        sponsImg: dr_ujj_img,
+        sponsAbt: `We are happy to announce Dr. Ujjwala's Skin Clinic as our healthcare partner for TEDxNITRaipur 6.0.`
     },
 
     {
         id: 15,
-        sponsImg: rocks_img
+        sponsImg: graceful,
+        sponsAbt: "We are happy to announce Graceful Media as our sponsor for TEDxNITRaipur 6.0."
     },
 
     {
         id: 16,
-        sponsImg: three_cheers
+        sponsImg: three_cheers,
+        sponsAbt: "We are happy to announce 3 cheers more as our sponsor for TEDxNITRaipur 6.0."
+    },
+
+    {
+        id: 17,
+        sponsImg: rocks_img,
+        sponsAbt: "We are happy to announce On the rocks as our sponsor for TEDxNITRaipur 6.0."
     }
     
 ]
